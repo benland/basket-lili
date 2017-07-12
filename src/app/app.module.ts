4df175ac4e2a07bae1d56556e86b30b631896c3a
Uri Shaked <ushaked@blackberry.com>
2017-07-12 14:16:48 +0300

    feat: material design (angular material)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { ItemsService } from './items.service';
  
+import 'hammerjs';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [
     ItemsService
