af6cee068c1f88a213722d16c39ecbca5ea61324
Uri Shaked <uri@salsa4fun.co.il>
2017-08-21 23:30:39 +0300

    test: Shufersal service

--- src/app/shufersal.service.spec.ts
+++ src/app/shufersal.service.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpModule, Http, BaseRequestOptions, XHRBackend } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
 
 import { ShufersalService } from './shufersal.service';
+import { HttpModule, Http, BaseRequestOptions, XHRBackend, Response, ResponseOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 describe('ShufersalService', () => {
   beforeEach(() => {
@@ -15,7 +15,21 @@ describe('ShufersalService', () => {
     });
   });
 
-  it('should be created', inject([ShufersalService], (service: ShufersalService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should parse shufersal API response correctly',
+    inject([ShufersalService, XHRBackend], (service: ShufersalService, backend: MockBackend) => {
+      const result = service.query('Test');
+      /* tslint:disable max-line-length */
+      backend.connectionsArray[0].mockRespond(new Response(new ResponseOptions({
+        body: `
+          var imgToLoad = [{"Url":"/_layouts/images/Shufersal/Images/Products_small/s_2826477.PNG","Id":"2826477","Alt":"שלגון בומבה","IsNF":false}]
+        `
+      })));
+      let res: any = null;
+      result.subscribe(response => res = response);
+      expect(res).toEqual([{
+        title: 'שלגון בומבה',
+        image: 'http://www.shufersal.co.il/_layouts/images/Shufersal/Images/Products_Large/z_2826477.PNG',
+        id: '2826477'
+      }]);
+    }));
 });
